e8da947ebd9f654b2f120911ab7cbb64fd568152
jbk77 <josephbk.117@hotmail.com>
2017-02-13 21:45:16 +0530

    Add project files.

---
+++ AchievementNLeaderBoardHelpTool/MainForm.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace AchievementNLeaderBoardHelpTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ascendingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.descendingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.noneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.DarkGray;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Underline);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.formatToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(20, 60);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 0, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(963, 27);
+            this.menuStrip1.Stretch = false;
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem1,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Orator Std", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.Color.Teal;
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(62, 25);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.BackColor = System.Drawing.Color.Teal;
+            this.openToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.BackColor = System.Drawing.Color.Teal;
+            this.saveToolStripMenuItem1.ForeColor = System.Drawing.Color.White;
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(180, 26);
+            this.saveToolStripMenuItem1.Text = "Save";
+            this.saveToolStripMenuItem1.Click += new System.EventHandler(this.saveToolStripMenuItem1_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.BackColor = System.Drawing.Color.Teal;
+            this.saveToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.saveToolStripMenuItem.Text = "Open Saved";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.BackColor = System.Drawing.Color.Teal;
+            this.exitToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ascendingToolStripMenuItem,
+            this.descendingToolStripMenuItem,
+            this.noneToolStripMenuItem});
+            this.formatToolStripMenuItem.Font = new System.Drawing.Font("Orator Std", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.formatToolStripMenuItem.ForeColor = System.Drawing.Color.Teal;
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(82, 25);
+            this.formatToolStripMenuItem.Text = "Format";
+            // 
+            // ascendingToolStripMenuItem
+            // 
+            this.ascendingToolStripMenuItem.BackColor = System.Drawing.Color.Teal;
+            this.ascendingToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.ascendingToolStripMenuItem.Name = "ascendingToolStripMenuItem";
+            this.ascendingToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.ascendingToolStripMenuItem.Text = "Ascending";
+            this.ascendingToolStripMenuItem.Click += new System.EventHandler(this.ascendingToolStripMenuItem_Click);
+            // 
+            // descendingToolStripMenuItem
+            // 
+            this.descendingToolStripMenuItem.BackColor = System.Drawing.Color.Teal;
+            this.descendingToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.descendingToolStripMenuItem.Name = "descendingToolStripMenuItem";
+            this.descendingToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.descendingToolStripMenuItem.Text = "Descending";
+            this.descendingToolStripMenuItem.Click += new System.EventHandler(this.descendingToolStripMenuItem_Click);
+            // 
+            // noneToolStripMenuItem
+            // 
+            this.noneToolStripMenuItem.BackColor = System.Drawing.Color.Teal;
+            this.noneToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.noneToolStripMenuItem.Name = "noneToolStripMenuItem";
+            this.noneToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.noneToolStripMenuItem.Text = "None";
+            this.noneToolStripMenuItem.Click += new System.EventHandler(this.noneToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.DetectUrls = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(20, 90);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ShortcutsEnabled = false;
+            this.richTextBox1.Size = new System.Drawing.Size(963, 137);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "CLICK FILE -> OPEN AND CHOOSE THE GENERATED .CS FILE THAT YOU GOT FROM GOOGLE PLA" +
+    "Y SERVICES";
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(69)))), ((int)(((byte)(78)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.button1.Location = new System.Drawing.Point(428, 233);
+            this.button1.MinimumSize = new System.Drawing.Size(100, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Set Up";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.Turquoise;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Font = new System.Drawing.Font("OCR A Std", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.MinimumSize = new System.Drawing.Size(120, 120);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(481, 183);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.listView1_ItemCheck);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Achievement";
+            this.columnHeader1.Width = 175;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Code";
+            this.columnHeader2.Width = 175;
+            // 
+            // listView2
+            // 
+            this.listView2.BackColor = System.Drawing.Color.PaleGreen;
+            this.listView2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView2.Font = new System.Drawing.Font("OCR A Std", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView2.FullRowSelect = true;
+            this.listView2.GridLines = true;
+            this.listView2.Location = new System.Drawing.Point(490, 3);
+            this.listView2.MinimumSize = new System.Drawing.Size(120, 120);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(482, 183);
+            this.listView2.TabIndex = 4;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Leaderboard";
+            this.columnHeader3.Width = 177;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Code";
+            this.columnHeader4.Width = 185;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.listView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listView2, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(23, 274);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(975, 189);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 471);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(700, 300);
+            this.Name = "Form1";
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow;
+            this.Style = MetroFramework.MetroColorStyle.Orange;
+            this.Text = "Google Play Set Up";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ascendingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem descendingToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ToolStripMenuItem noneToolStripMenuItem;
+    }
+}
+
---
+++ AchievementNLeaderBoardHelpTool/MainForm.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using MetroFramework.Forms;
+
+namespace AchievementNLeaderBoardHelpTool
+{
+
+
+    public partial class MainForm : MetroForm
+    {
+        OpenFileDialog ofd;
+        string[] achievements;
+        List<int> achievementsChecked;
+        List<int> checkedACH;
+
+
+        public MainForm()
+        {            
+            InitializeComponent();            
+        }
+        
+        void setUpAction() {
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+
+
+            string resourceData = richTextBox1.Text;          
+            
+            int endIndex = resourceData.LastIndexOf('}');
+            int firstIndex = resourceData.IndexOf('{') + 1;
+            resourceData = resourceData.Substring(firstIndex, endIndex - firstIndex);
+            resourceData = resourceData.Replace("// <GPGSID>", "");
+            resourceData = resourceData.Replace("        ", "");
+            resourceData = resourceData.Replace(";", "");
+            // Console.WriteLine("New resource : " + Environment.NewLine + resourceData + Environment.NewLine);
+            int numberOfResources = 0;
+            resourceData = resourceData.Trim();
+            char[] temp = resourceData.ToCharArray();
+            for (int i = 0; i < resourceData.Length; i++)
+            {
+                if (temp[i] == '\n')
+                    numberOfResources++;
+            }
+
+            Console.WriteLine("Lines = " + numberOfResources);
+            string[] seperate_res = resourceData.Split('\n', '=');
+
+            int numberOfACH = 0;
+            int numberOfLEA = 0;
+            for (int i = 0; i < seperate_res.Length; i++)
+            {
+                if (seperate_res[i].Contains("achievement"))
+                    numberOfACH++;
+                if (seperate_res[i].Contains("leaderboard"))
+                    numberOfLEA++;
+            }
+
+            string[,] achievements = new string[numberOfACH, 2];
+            string[,] leaderBoard = new string[numberOfLEA, 2];
+            achievementsChecked = new List<int>(numberOfACH);
+
+            int _numACH = 0, _numLEA = 0;
+
+            for (int i = 0; i < seperate_res.Length; i++)
+            {
+                if (seperate_res[i].Contains("achievement") || seperate_res[i].Contains("leaderboard"))
+                {
+                    seperate_res[i] = seperate_res[i].Remove(0, 20);
+                    if (seperate_res[i].Contains("achievement"))
+                    {
+                        achievements[_numACH, 0] = seperate_res[i];
+                        achievements[_numACH, 1] = seperate_res[i + 1];
+                        _numACH++;
+                    }
+                    else if (seperate_res[i].Contains("leaderboard"))
+                    {
+                        leaderBoard[_numLEA, 0] = seperate_res[i];
+                        leaderBoard[_numLEA, 1] = seperate_res[i + 1];
+                        _numLEA++;
+                    }
+
+                }
+            }
+
+
+            Console.WriteLine("All The Achievements : ");
+            for (int i = 0; i < numberOfACH; i++)
+                Console.WriteLine("::" + achievements[i, 0]);
+            Console.WriteLine("All The Leaderboards : ");
+            for (int i = 0; i < numberOfLEA; i++)
+                Console.WriteLine("::" + leaderBoard[i, 0]);
+
+            for (int i = 0; i < numberOfACH; i++)
+            {
+                ListViewItem item = new ListViewItem(achievements[i, 0].Remove(0, 12).Replace("_", " "));
+                item.SubItems.Add(achievements[i, 1].Replace("\"", ""));
+                listView1.Items.Add(item);
+            }
+            for (int i = 0; i < numberOfLEA; i++)
+            {
+                ListViewItem item = new ListViewItem(leaderBoard[i, 0].Remove(0, 12).Replace("_", " "));
+                item.SubItems.Add(leaderBoard[i, 1].Replace("\"", ""));
+                listView2.Items.Add(item);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                setUpAction();
+            }
+            catch {
+                MessageBox.Show("Error : No File Opened");
+            }           
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            DialogResult result = MessageBox.Show("Are You Sure", "Leaving The Application",MessageBoxButtons.YesNo);
+            if(result==DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK) {
+                richTextBox1.Text = System.IO.File.ReadAllText(ofd.FileName);                
+            }
+        }
+
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "GPH File(*.pgh;)|*.GPH;";
+                string[] content = new string[achievementsChecked.Count + 1];
+                if (ofd.FileName != null)
+                    content[0] = ofd.FileName;
+
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    int j = 1;
+                    foreach (var item in achievementsChecked)
+                    {
+                        content[j] = item.ToString();
+                        j++;
+                    }
+                    System.IO.File.WriteAllLines(sfd.FileName, content);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("You Have Not Yet Opened A File");
+            }            
+        }
+
+        private void listView1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            MessageBox.Show("Checked : " + e.Index);
+            achievementsChecked.Add(e.Index);            
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog svofd = new OpenFileDialog();
+            svofd.Filter = "GPH File(*.pgh;)|*.GPH;";
+            if (svofd.ShowDialog() == DialogResult.OK)
+            {
+                StringBuilder sb = new StringBuilder();
+                int numOfChecks = 0;
+                using (StreamReader sr = new StreamReader(svofd.FileName))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        sb.Append(sr.ReadLine() + '\n');                        
+                    }
+                }
+                for (int i = 0; i < sb.Length; i++) {
+                    if (sb.ToString().ToCharArray()[i] == '\n')
+                        numOfChecks++;
+                }
+                string[] path = sb.ToString().Split('\n');
+                Console.WriteLine("FIle ::" + path[0] + " number of lines (checks) : " + numOfChecks);
+                richTextBox1.Text = System.IO.File.ReadAllText(path[0]);
+                setUpAction();
+                
+                
+                checkedACH = new List<int>(numOfChecks);
+                for (int i = 1; i < numOfChecks; i++)
+                {
+                    checkedACH.Add(int.Parse(path[i]));
+                }
+                foreach (int i in checkedACH){
+                    Console.WriteLine("checks : " + i);
+                }
+
+                Console.WriteLine("Number of list items = " + listView1.Items.Count);
+
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+                    if(checkedACH.Contains(i))
+                        listView1.Items[i].Checked = true;
+                }
+
+            }
+        }
+
+        private void ascendingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView2.Sorting = SortOrder.Ascending;
+        }
+
+        private void descendingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView2.Sorting = SortOrder.Descending;
+        }
+
+        private void noneToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView2.Sorting = SortOrder.None;
+            setUpAction();
+        }
+    }
+}
